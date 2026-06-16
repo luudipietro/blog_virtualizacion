bdc23016491b4f32cff4419b3c88ead8e52b53c2
PC Luciano Di Pietro <huesodipietro@gmail.com>
2026-06-16 15:17:45 -0300

    string con longitud en usuario

--- domain/usuario.py
+++ domain/usuario.py
@@ -4,5 +4,5 @@ from database import Base
 class Usuario(Base):
     __tablename__ = "usuarios"
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
+    username = Column(String(255), unique=True, index=True)
+    hashed_password = Column(String(1000))
