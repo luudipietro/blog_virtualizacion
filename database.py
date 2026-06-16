167c27c3fd754f1a1c17bba42242884edf79b6d6
PC Luciano Di Pietro <huesodipietro@gmail.com>
2026-06-16 15:00:34 -0300

    configurar db

--- database.py
+++ database.py
@@ -2,6 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+from dotenv import load_dotenv
+load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL","sqlite:///./blog.db")
 
 # SQLite requiere un argumento especial que MariaDB no acepta
